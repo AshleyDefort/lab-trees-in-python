a86ddef031dfefb9ae59aaef14a6d707952306f5
Ashley <ashleydefort@outlook.es>
2024-03-17 19:05:04 -0500

    Add comments to image_widget.py

--- image_widget.py
+++ image_widget.py
@@ -2,11 +2,13 @@ import customtkinter as ctk
 from tkinter import Canvas
 from PIL import Image, ImageTk
 
+# Clase que representa un widget de imagen
 class ImageWidget(Canvas):
   def __init__(self, parent):
     super().__init__(master=parent, bd=0, highlightthickness=0, relief='ridge')
     self.grid(row=0, column=1, sticky='nsew')
 
+  # Método que muestra una imagen en el widget
   def show_image(self, image_path):
     image = Image.open(image_path)
     self.image = ImageTk.PhotoImage(image) 
